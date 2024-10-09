821b1c8207aef9f3376d2472a7cbab807284afcf
Ytalo Vasconcelos <Ytalo Vasconcelos@DESKTOP-A0VNITD>
2024-10-09 00:11:16 -0300

    Adicionar arquivos de projeto.

---
+++ Aprendizado/Livros.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+namespace Aprendizado
+{
+    class Livros
+    {
+        // int = numero inteiros
+        // double = numeros reais
+        // string = Frase/Palavra/Texto
+        // float = 4.5f
+        // char = apenas uma letra
+        // get = buscar,ler,adicionar
+        // set = alterar/modificar
+        // private set = so pode modificar dentro da classe
+        // private get = so pode ser lido ou adicionar dentro da classe
+        // if/else = se/se nao (condição)
+        // for = para todo (repetição)
+        // ++ = incremento/adição
+        // -- = decremento/diminuição
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public double Preco { get; private set; }
+
+        public Livros(int id, string nome, string autor, double preco)
+        {
+            Id = id;
+            Nome = nome;
+            Autor = autor;
+            Preco = preco;
+        }
+        public override string ToString()
+        {
+            return "ID: " + Id
+            + ": " + Nome
+            + ", " + Autor
+            + ", Valor: R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Aprendizado/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Aprendizado
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Quantos livros deseja adicionar? ");
+            int qtd = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            List<Livros> livros = new List<Livros>();
+
+            for (int i = 1; i <= qtd; i++)
+            {
+
+                Console.WriteLine("Livro #" + i + ": ");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Autor: ");
+                string autor = Console.ReadLine();
+                Console.Write("Preço: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+
+                livros.Add(new Livros(id, nome, autor, preco));
+            }
+            Console.WriteLine("Livros cadastrados :");
+            Console.WriteLine();
+            foreach (Livros obj in livros)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine();
+            Console.Write("Digite o Id de um livro: ");
+            int searchId = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Livros livro = livros.Find(x => x.Id == searchId);
+            if (livro != null)
+            {
+                Console.WriteLine("Deseja remover? S/N ");
+                char resp = char.Parse(Console.ReadLine());
+
+                if (resp == 'S' || resp == 's')
+                {
+                    livros.Remove(livro);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Id inválido");
+            }
+            Console.WriteLine();
+            foreach (Livros obj in livros)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
